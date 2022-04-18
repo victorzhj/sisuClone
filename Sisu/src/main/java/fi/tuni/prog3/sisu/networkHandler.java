109676fcf2d4c7d6b4508802d301b448d08d1191
Aleksi Hietamäki <aleksi.hietamaki@tuni.fi>
2022-04-18 19:44:21 +0300

    fixed gson import

--- Sisu/src/main/java/fi/tuni/prog3/sisu/networkHandler.java
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/networkHandler.java
@@ -1,29 +1,38 @@
 package fi.tuni.prog3.sisu;
 import java.net.URI;
 import java.net.http.*;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.security.KeyStore.Entry;
 import java.util.Map;
 import java.util.TreeMap;
-
 public class networkHandler {
     private String sisuUrl = "https://sis-tuni.funidata.fi/kori/api/";
     private String tuniId = "universityId=tuni-university-root-id";
     private String defaultPeriod = "curriculumPeriodId=uta-lvv-2021";
+    private String defaultLimit = "limit=1000";
     private TreeMap<String, String> endpoints = new TreeMap<String,String>();
     private HttpClient client = HttpClient.newHttpClient();
+
     public networkHandler(){
         endpoints.put("moduleSearch", "module-search");
     }
+    
+    public degreeData getDegreePrograms(){
+        HttpRequest req = moduleSearch(defaultPeriod, tuniId, "moduleType=DegreeProgramme", defaultLimit);
+        return client.sendAsync(req, BodyHandlers.ofString())
+        .thenApply(HttpResponse::body)
+        .thenAccept((res)->{
+            return degreeData(res);
 
-    public getDegreePrograms(){
-
+        });
     }
-
-    public moduleSearch(){
-
+    
+    public HttpRequest moduleSearch(String period, String universityId, String moduleType,String limit){
+        var uri = URI.create(sisuUrl+endpoints.get("moduleSearch")+"?"+period+"&"+universityId+"&"+moduleType+"&"+limit);
+        return createGetRequest(uri);
     }
-    private HttpRequest createGetRequest(String url){
-        return HttpRequest.newBuilder(URI.create(url))
+    private HttpRequest createGetRequest(URI url){
+        return HttpRequest.newBuilder(url)
         .header("accept","application/json")
         .build();
     }
--- Sisu/src/main/java/module-info.java
+++ Sisu/src/main/java/module-info.java
@@ -1,4 +1,6 @@
 module fi.tuni.prog3.sisu {
     requires javafx.controls;
+    requires java.net.http;
+    requires com.google.gson;
     exports fi.tuni.prog3.sisu;
 }

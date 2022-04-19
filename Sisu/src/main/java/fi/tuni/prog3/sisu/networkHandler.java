2aa79a1ffe4ce86bb1a19c454dc350478da15868
Aleksi Hietamäki <aleksi.hietamaki@tuni.fi>
2022-04-19 18:44:58 +0300

    networking endpoint communication done

--- Sisu/src/main/java/fi/tuni/prog3/sisu/networkHandler.java
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/networkHandler.java
@@ -2,32 +2,59 @@ package fi.tuni.prog3.sisu;
 import java.net.URI;
 import java.net.http.*;
 import java.net.http.HttpResponse.BodyHandlers;
-import java.security.KeyStore.Entry;
-import java.util.Map;
 import java.util.TreeMap;
 public class networkHandler {
     private String sisuUrl = "https://sis-tuni.funidata.fi/kori/api/";
     private String tuniId = "universityId=tuni-university-root-id";
     private String defaultPeriod = "curriculumPeriodId=uta-lvv-2021";
     private String defaultLimit = "limit=1000";
+    private String groupIdSearch = "by-group-id?";
     private TreeMap<String, String> endpoints = new TreeMap<String,String>();
     private HttpClient client = HttpClient.newHttpClient();
 
     public networkHandler(){
         endpoints.put("moduleSearch", "module-search");
+        endpoints.put("modules", "modules/");
+        endpoints.put("courseUnits", "course-units/");
     }
     
-    public degreeData getDegreePrograms(){
+    public String getDegreePrograms(){
         HttpRequest req = moduleSearch(defaultPeriod, tuniId, "moduleType=DegreeProgramme", defaultLimit);
         return client.sendAsync(req, BodyHandlers.ofString())
-        .thenApply(HttpResponse::body)
-        .thenAccept((res)->{
-            return degreeData(res);
-
-        });
+        .thenApply(HttpResponse::body).join();
     }
-    
-    public HttpRequest moduleSearch(String period, String universityId, String moduleType,String limit){
+    public String getModuleById(String id){
+        HttpRequest req = moduleGet(id);
+        return client.sendAsync(req, BodyHandlers.ofString())
+        .thenApply(HttpResponse::body).join();
+    }
+
+    public String getModuleByGroupId(String id){
+        HttpRequest req = moduleIdGet(id);
+        return client.sendAsync(req, BodyHandlers.ofString())
+        .thenApply(HttpResponse::body).join();
+    }
+    public String getCourseByGroupId(String id){
+        HttpRequest req = courseIdGet(id);
+        return client.sendAsync(req, BodyHandlers.ofString())
+        .thenApply(HttpResponse::body).join();
+    }
+
+    private HttpRequest courseIdGet(String id) {
+        var uri = URI.create(sisuUrl+endpoints.get("courseUnits")+groupIdSearch+id+"&"+tuniId);
+        return createGetRequest(uri);
+    }
+
+    private HttpRequest moduleIdGet(String id) {
+        var uri = URI.create(sisuUrl+endpoints.get("modules")+groupIdSearch+id+"&"+tuniId);
+        return createGetRequest(uri);
+    }
+
+    private HttpRequest moduleGet(String id){
+        var uri = URI.create(sisuUrl+endpoints.get("modules")+id);
+        return createGetRequest(uri);
+    }
+    private HttpRequest moduleSearch(String period, String universityId, String moduleType,String limit){
         var uri = URI.create(sisuUrl+endpoints.get("moduleSearch")+"?"+period+"&"+universityId+"&"+moduleType+"&"+limit);
         return createGetRequest(uri);
     }
--- Sisu/src/main/java/module-info.java
+++ Sisu/src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module fi.tuni.prog3.sisu {
     requires javafx.controls;
-    requires java.net.http;
+    requires transitive java.net.http;
     requires com.google.gson;
     exports fi.tuni.prog3.sisu;
 }

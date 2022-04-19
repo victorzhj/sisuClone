82c29fb1329851d6dcf7351587f6e13f9760da9d
Aleksi Hietamäki <aleksi.hietamaki@tuni.fi>
2022-04-19 18:52:56 +0300

    added comments for networkHandler

--- Sisu/src/main/java/fi/tuni/prog3/sisu/networkHandler.java
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/networkHandler.java
@@ -4,60 +4,107 @@ import java.net.http.*;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.TreeMap;
 public class networkHandler {
-    private String sisuUrl = "https://sis-tuni.funidata.fi/kori/api/";
-    private String tuniId = "universityId=tuni-university-root-id";
-    private String defaultPeriod = "curriculumPeriodId=uta-lvv-2021";
-    private String defaultLimit = "limit=1000";
-    private String groupIdSearch = "by-group-id?";
-    private TreeMap<String, String> endpoints = new TreeMap<String,String>();
+    private static final String sisuUrl = "https://sis-tuni.funidata.fi/kori/api/";
+    private static final String tuniId = "universityId=tuni-university-root-id";
+    private static final String defaultPeriod = "curriculumPeriodId=uta-lvv-2021";
+    private static final String defaultLimit = "limit=1000";
+    private static final String groupIdSearch = "by-group-id?";
+    private static final TreeMap<String, String> endpoints = new TreeMap<String,String>();
     private HttpClient client = HttpClient.newHttpClient();
 
+    /**
+     * Default constructor for networkHandler
+     */
     public networkHandler(){
         endpoints.put("moduleSearch", "module-search");
         endpoints.put("modules", "modules/");
         endpoints.put("courseUnits", "course-units/");
     }
     
+    
+    /** 
+     * Retrieves all degree programmes from sisu api with tuni id from 2021 curriculum period
+     * @return String Containing data from the request
+     */
     public String getDegreePrograms(){
         HttpRequest req = moduleSearch(defaultPeriod, tuniId, "moduleType=DegreeProgramme", defaultLimit);
         return client.sendAsync(req, BodyHandlers.ofString())
         .thenApply(HttpResponse::body).join();
     }
+    
+    /** 
+     * Searches module by id.
+     * @param id Of the module to be searched
+     * @return String Containing data from the request
+     */
     public String getModuleById(String id){
         HttpRequest req = moduleGet(id);
         return client.sendAsync(req, BodyHandlers.ofString())
         .thenApply(HttpResponse::body).join();
     }
 
+    
+    /** 
+     *  Searches module by group-id
+     * @param id Of the module to be searched
+     * @return String Containing data from the request
+     */
     public String getModuleByGroupId(String id){
         HttpRequest req = moduleIdGet(id);
         return client.sendAsync(req, BodyHandlers.ofString())
         .thenApply(HttpResponse::body).join();
     }
+    
+    /** 
+     * Searches course by group-id
+     * @param id Of the course to be searched
+     * @return String Containing data from the request
+     */
     public String getCourseByGroupId(String id){
         HttpRequest req = courseIdGet(id);
         return client.sendAsync(req, BodyHandlers.ofString())
         .thenApply(HttpResponse::body).join();
     }
 
+    
+    /** 
+     * 
+     * @hidden
+     */
     private HttpRequest courseIdGet(String id) {
         var uri = URI.create(sisuUrl+endpoints.get("courseUnits")+groupIdSearch+id+"&"+tuniId);
         return createGetRequest(uri);
     }
 
+    
+    /** 
+     * @hidden
+     */
     private HttpRequest moduleIdGet(String id) {
         var uri = URI.create(sisuUrl+endpoints.get("modules")+groupIdSearch+id+"&"+tuniId);
         return createGetRequest(uri);
     }
 
+    
+    /** 
+     * @hidden
+     */
     private HttpRequest moduleGet(String id){
         var uri = URI.create(sisuUrl+endpoints.get("modules")+id);
         return createGetRequest(uri);
     }
+    
+    /** 
+     * @hidden
+     */
     private HttpRequest moduleSearch(String period, String universityId, String moduleType,String limit){
         var uri = URI.create(sisuUrl+endpoints.get("moduleSearch")+"?"+period+"&"+universityId+"&"+moduleType+"&"+limit);
         return createGetRequest(uri);
     }
+    
+    /** 
+     * @hidden
+     */
     private HttpRequest createGetRequest(URI url){
         return HttpRequest.newBuilder(url)
         .header("accept","application/json")

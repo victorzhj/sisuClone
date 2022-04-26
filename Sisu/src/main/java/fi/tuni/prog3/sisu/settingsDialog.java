95f4c83c2f61ae3f4488f85520e9d7305ae9d4ed
Aleksi Hietamäki <aleksi.hietamaki@tuni.fi>
2022-04-26 12:37:10 +0300

    added some concurrency  to settingsdialog to not block the gui thread

--- Sisu/src/main/java/fi/tuni/prog3/sisu/settingsDialog.java
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/settingsDialog.java
@@ -1,9 +1,11 @@
 package fi.tuni.prog3.sisu;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextInputDialog;
 import javafx.scene.control.TreeItem;
@@ -19,13 +21,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.TreeMap;
-public class settingsDialog extends Application{
+public class settingsDialog extends Application implements Runnable{
     private TreeView<treeItems> degreeProgramsList =new TreeView<treeItems>();
     private networkHandler networker = new networkHandler();
     private Button confirmButton = new Button();
     private TextField nameField = new TextField();
     private TextField studentNumber = new TextField();
     private GridPane settingsGrid = new GridPane();
+    private ProgressBar loadingBar = new ProgressBar();
+    double downloadProgress = 0;
     private class treeItems{
         treeItems(String name, String id){
             this.name = name;
@@ -45,14 +49,19 @@ public class settingsDialog extends Application{
             return getName();
         }
     }
-
+    
 
     @Override
     public void start(Stage mainStage) throws Exception {
+
+        Thread fillTreeView = new Thread(this);
+        fillTreeView.start();
+
         nameField.setPrefSize(200, 50);
         confirmButton.setPrefSize(100, 50);
         studentNumber.setMinSize(200, 50);
         degreeProgramsList.setPrefSize(400, 200);
+        loadingBar.setPrefWidth(400);
         studentNumber.setText("Enter student number");
         nameField.setText("Enter name");
         confirmButton.setText("Confirm");
@@ -60,16 +69,31 @@ public class settingsDialog extends Application{
         settingsGrid.add(studentNumber, 0, 1,1, 1);
         settingsGrid.add(degreeProgramsList, 2, 0, 1, 3);
         settingsGrid.add(confirmButton, 0,2, 1 ,1);
+        settingsGrid.add(loadingBar, 2, 4, 3, 1);
         confirmButton.setAlignment(Pos.CENTER);
-        TreeItem<treeItems> rootItem = new TreeItem<treeItems>();
-
-        rootItem.setValue(new treeItems("DegreeProgrammes", "NO-ID"));
-        degreeProgramsList.setRoot(rootItem);
-
-        var degrees = DegreesData.getDegreesInformation();
         var scene = new Scene(settingsGrid);
         mainStage.setScene(scene);
         mainStage.show();
+        
+
+        
+        }
+        
+        
+    
+
+    public static void main(String[] args){
+        launch();
+    }
+
+    @Override
+    public void run() {
+        TreeItem<treeItems> rootItem = new TreeItem<treeItems>();
+        degreeProgramsList.setRoot(rootItem);
+        rootItem.setValue(new treeItems("DegreeProgrammes", "NO-ID"));
+        var degrees = DegreesData.getDegreesInformation();
+        var maxProgress = degrees.values().size();
+        System.out.println(maxProgress);
         for(var degree : degrees.values()){
             TreeItem<treeItems> branch = new TreeItem<treeItems>();
             branch.setValue(new treeItems(degree.get("name"), degree.get("groupId")));
@@ -86,13 +110,12 @@ public class settingsDialog extends Application{
                 branch.getChildren().add(subBranch);
             }
             rootItem.getChildren().add(branch);
+        downloadProgress++;
+        Platform.runLater(new Runnable() {
+            @Override public void run(){
+                loadingBar.setProgress(downloadProgress/maxProgress);
+            }
+        });
         }
-        degreeProgramsList.setMaxSize(400, 400);
-        
-        
-    }
-
-    public static void main(String[] args){
-        launch();
     }
 }
